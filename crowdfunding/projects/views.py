f18355365443d99f0c48658a58a3188021604df3
Soda De <soda.demoncerf@gmail.com>
2023-01-21 10:59:13 +1100

    BeforeProjectGetsWrecked
    Starting customization

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -3,9 +3,11 @@ from rest_framework.response import Response
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
 from django.http import Http404
-from rest_framework import status, generics 
+from rest_framework import status, generics, permissions
+from .permissions import IsOwnerOrReadOnly
 
 class ProjectList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     def get(self, request):
         projects = Project.objects.all() #retrieves all projects in database
         serializer = ProjectSerializer(projects, many=True)
@@ -27,9 +29,15 @@ class ProjectList(APIView):
             )
 
 class ProjectDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
     def get_object(self, pk):
         try:
             return Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request, project)
+            return project
         except Project.DoesNotExist:
             raise Http404
 
@@ -38,6 +46,20 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data 
+        serializer = ProjectDetailSerializer(
+            instance = project,
+            data = data,
+            partial = True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        
 
 class PledgeList(generics.ListCreateAPIView):
     queryset = Pledge.objects.all()
